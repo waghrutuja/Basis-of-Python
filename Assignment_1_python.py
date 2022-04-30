3684e9bee3e9d1959b443e0365ad2275d0273d72
waghrutuja <101442759+waghrutuja@users.noreply.github.com>
2022-04-30 10:22:50 +0530

    Add files via upload

---
+++ Assignment_1_python.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+A=["WaghRutuja",9157,"TYBSC"]
+B=["SP college","pune","tilak road"]
+
+print(A)
+print(B)
+A.append("statistics")
+print(A)
+A.extend([1,2,3])
+print(A)
+A.insert(4,5)
+print(A)
+A.remove(2)
+print(A)
+A.pop(3)
+print(A)
+print(A[:4])      #slice
+A.reverse()
+print(A)
+print(len(A))
+A.append(5)
+print(A)
+print(A.count(5))
+print(A+B)    #concatenate
+print(A*2)
+A.clear()
+print(A)
+
+
+# name :- Wagh Rutuja Deepak
+# rollno :- 9157 
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+a=("WaghRutuja",9157,"TYBSC")
+b=("SP college","pune","tilak road")
+
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+print(a[2])
+print(a+b)
+print(a*3)
+print(9181 in a)
+print(5 in a)
+c=(a,b)
+print(c)
+print(a[1:])
+print(len(a))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
